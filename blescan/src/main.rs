c8219058de3d2346b7e87a9f329694830904bfd4
Mike Moran <mike@houseofmoran.com>
2023-07-15 18:55:23 +0100

    compute a signature based on manufacturer data in local_name not provided

--- blescan/src/main.rs
+++ blescan/src/main.rs
@@ -54,7 +54,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
 fn find_signature(properties: &PeripheralProperties) -> Option<String> {
     if let Some(local_name) = &properties.local_name {
         Some(local_name.clone())
-    } else {
-        None
+    } else { 
+        if !&properties.manufacturer_data.is_empty() {
+            let mut context = md5::Context::new();
+            let mut manufacturer_ids: Vec<&u16> = properties.manufacturer_data.keys().collect();
+            manufacturer_ids.sort();
+            for manufacturer_id in manufacturer_ids {
+                let arbitrary_data = properties.manufacturer_data[manufacturer_id].clone();
+                context.consume(arbitrary_data);
+            }
+            let digest = context.compute();
+            Some(format!("{:x}", digest))
+        }
+        else {
+            None
+        }
     }
 }
