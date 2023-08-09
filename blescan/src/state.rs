877cce4a9a33f568b0428f1dcd76b12dbd24915d
Mike Moran <mike@houseofmoran.com>
2023-08-09 16:17:13 +0200

    extract `device_state`

--- blescan/src/state.rs
+++ blescan/src/state.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::{signature::Signature, discover::DiscoveryEvent, snapshot::Snapshot};
+use crate::{signature::Signature, discover::DiscoveryEvent, snapshot::Snapshot, device_state::DeviceState};
 
 pub struct State {
     state: HashMap<Signature, DeviceState>
@@ -30,28 +30,7 @@ impl State {
     }
 }
 
-#[derive(PartialEq, Debug, Clone)]
-pub struct DeviceState {
-    pub signature: Signature,
-    pub rssi: i16,
-}
 
-impl DeviceState {
-    pub fn new(signature: Signature, rssi: i16) -> DeviceState {
-        DeviceState { signature, rssi }
-    }
-
-    pub fn from_event(event: &DiscoveryEvent) -> DeviceState {
-        DeviceState { 
-            signature: event.signature.clone(), 
-            rssi: event.rssi
-        }
-    }
-
-    pub fn update(&mut self, rssi: i16) {
-        self.rssi = rssi;
-    }
-}
 
 
 #[cfg(test)]
