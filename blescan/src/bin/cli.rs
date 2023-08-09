ccb2a0f63b255f895522285435ac79ad5be498b3
Mike Moran <mike@houseofmoran.com>
2023-08-09 16:04:19 +0200

    extract `discover_btleplug` and use in `cli`

--- blescan/src/bin/cli.rs
+++ blescan/src/bin/cli.rs
@@ -1,13 +1,17 @@
 use std::error::Error;
-use blescan::scanner::Scanner;
+
+use blescan::{discover_btleplug::Scanner, state::State};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
 
     let mut scanner = Scanner::new().await?;
+    let mut state = State::default();
     loop {
-        scanner.scan().await?;
-        println!("{}", scanner);
+        println!("{}", state.snapshot());
+        let events = scanner.scan().await?;
+        state.discover(events);
     }
 }
+
