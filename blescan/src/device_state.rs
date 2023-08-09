3790636ed0505b7d7080284ab01833bacc4e8f3a
Mike Moran <mike@houseofmoran.com>
2023-08-09 17:05:42 +0200

    re-add sorting by age

--- blescan/src/device_state.rs
+++ blescan/src/device_state.rs
@@ -1,24 +1,29 @@
+use chrono::{DateTime, Utc};
+
 use crate::{signature::Signature, discover::DiscoveryEvent};
 
 #[derive(PartialEq, Debug, Clone)]
 pub struct DeviceState {
+    pub date_time: DateTime<Utc>,
     pub signature: Signature,
     pub rssi: i16,
 }
 
 impl DeviceState {
-    pub fn new(signature: Signature, rssi: i16) -> DeviceState {
-        DeviceState { signature, rssi }
+    pub fn new(date_time: DateTime<Utc>, signature: Signature, rssi: i16) -> DeviceState {
+        DeviceState { date_time, signature, rssi }
     }
 
     pub fn from_event(event: &DiscoveryEvent) -> DeviceState {
-        DeviceState { 
+        DeviceState {
+            date_time: event.date_time,
             signature: event.signature.clone(), 
             rssi: event.rssi
         }
     }
 
-    pub fn update(&mut self, rssi: i16) {
-        self.rssi = rssi;
+    pub fn update(&mut self, event: &DiscoveryEvent) {
+        self.date_time = event.date_time;
+        self.rssi = event.rssi;
     }
 }
--- blescan/src/discover.rs
+++ blescan/src/discover.rs
@@ -1,12 +1,15 @@
+use chrono::{DateTime, Utc};
+
 use crate::signature::Signature;
 
 pub struct DiscoveryEvent {
+    pub date_time: DateTime<Utc>,
     pub signature: Signature,
     pub rssi: i16,
 }
 
 impl DiscoveryEvent {
-    pub fn new(signature: Signature, rssi: i16) -> DiscoveryEvent {
-        DiscoveryEvent { signature, rssi }
+    pub fn new(date_time: DateTime<Utc>, signature: Signature, rssi: i16) -> DiscoveryEvent {
+        DiscoveryEvent { date_time, signature, rssi }
     }
 }
