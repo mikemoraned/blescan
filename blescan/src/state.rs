0558f961a0ef0e4cb14ec2811969d4d559fce22b
Mike Moran <mike@houseofmoran.com>
2023-08-09 15:43:25 +0200

    implement basics in state

--- blescan/src/state.rs
+++ blescan/src/state.rs
@@ -24,29 +24,43 @@ impl State {
     pub fn discover(&mut self, events: Vec<DiscoveryEvent>) {
         for event in events {
             self.state.entry(event.signature.clone())
-                            .or_insert(DeviceState::new(event.signature.clone()));
+                .and_modify(|s: &mut DeviceState| s.update(event.rssi))
+                .or_insert(DeviceState::from_event(&event));
         }
     }
 }
 
 #[derive(PartialEq, Debug, Clone)]
 pub struct DeviceState {
-    pub signature: Signature
+    pub signature: Signature,
+    pub rssi: i16,
 }
 
 impl DeviceState {
-    pub fn new(signature: Signature) -> DeviceState {
-        DeviceState { signature }
+    pub fn new(signature: Signature, rssi: i16) -> DeviceState {
+        DeviceState { signature, rssi }
+    }
+
+    pub fn from_event(event: &DiscoveryEvent) -> DeviceState {
+        DeviceState { 
+            signature: event.signature.clone(), 
+            rssi: event.rssi
+        }
+    }
+
+    pub fn update(&mut self, rssi: i16) {
+        self.rssi = rssi;
     }
 }
 
 pub struct DiscoveryEvent {
-    pub signature: Signature
+    pub signature: Signature,
+    pub rssi: i16,
 }
 
 impl DiscoveryEvent {
-    pub fn new(signature: Signature) -> DiscoveryEvent {
-        DiscoveryEvent { signature }
+    pub fn new(signature: Signature, rssi: i16) -> DiscoveryEvent {
+        DiscoveryEvent { signature, rssi }
     }
 }
 
@@ -65,14 +79,24 @@ mod test {
     #[test]
     fn initial_discovery() {
         let mut state = State::default();
-        state.discover(vec![DiscoveryEvent::new(Signature::Named("Device 1".to_string()))]);
-        assert_eq!(state.snapshot(), vec![DeviceState::new(Signature::Named("Device 1".to_string()))]);
+        state.discover(
+            vec![DiscoveryEvent::new(Signature::Named("Device 1".to_string()), -10)]
+        );
+        assert_eq!(state.snapshot(), 
+            vec![DeviceState::new(Signature::Named("Device 1".to_string()), -10)]
+        );
     }
 
     #[test]
     fn updated_state() {
         let mut state = State::default();
-        state.discover(vec![DiscoveryEvent::new(Signature::Named("Device 1".to_string()))]);
-        assert_eq!(state.snapshot(), vec![DeviceState::new(Signature::Named("Device 1".to_string()))]);
+        state.discover(
+            vec![DiscoveryEvent::new(Signature::Named("Device 1".to_string()), -10)]
+        );
+        state.discover(
+            vec![DiscoveryEvent::new(Signature::Named("Device 1".to_string()), -20)]
+        );
+        assert_eq!(state.snapshot(), 
+            vec![DeviceState::new(Signature::Named("Device 1".to_string()), -20)]);
     }
 }
