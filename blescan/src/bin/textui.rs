a8bbc54f7299b81ee0aedd36c1e5c1c1a710e577
Mike Moran <mike@houseofmoran.com>
2023-08-10 18:19:32 +0200

    Order display by Age and Volume; show Volume change (#8)
    * define age as being age relative to now (how long ago it was last seen)
    * order by age (latest update) then volume (loudest)
    * add Context area

--- blescan/src/bin/textui.rs
+++ blescan/src/bin/textui.rs
@@ -1,17 +1,17 @@
 use std::{
     io::{self, Stdout},
-    time::Duration, error::Error,
+    time::Duration, error::Error, rc::Rc,
 };
 
 use anyhow::{Context, Result};
-use blescan::{discover_btleplug::Scanner, state::State, signature::Signature};
-use chrono::{Utc, DurationRound};
+use blescan::{discover_btleplug::Scanner, state::State, signature::Signature, snapshot::{Snapshot, RssiComparison}};
+use chrono::{Utc, DateTime};
 use crossterm::{
     event::{self, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use ratatui::{prelude::*, widgets::{List, ListItem}};
+use ratatui::{prelude::*, widgets::{List, ListItem, Paragraph}};
 use ratatui::{
     layout::{Constraint, Direction, Layout},
     widgets::{Block, Borders}
@@ -45,86 +45,104 @@ async fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Bo
 
     let mut scanner = Scanner::new().await?;
     let mut state = State::default();
-    let start = Utc::now().duration_round(chrono::Duration::seconds(1)).unwrap();
+    let start = Utc::now();
+    let mut previous_snapshot = Snapshot::default();
     loop {
+        let current_snapshot = state.snapshot();
         terminal.draw(|f| {
-            let ordered_by_age = state.snapshot().order_by_age_oldest_last();
-            let with_age = ordered_by_age.compared_to(start);
-            let (named_items, anon_items)   
-                = with_age.iter().fold((Vec::new(), Vec::new()), 
-                    |
-                        (named, anon), 
-                        (state, comparison)
-                    | {
-                    let age_summary 
-                        = format_duration(comparison.relative_age.truncate_to_seconds().to_std().unwrap());
-                    match &state.signature {
-                        Signature::Named(n) => {
-                            let item 
-                                = ListItem::new(format!("{:<32}[{}]:{:>4}", 
-                                    n, age_summary, state.rssi));
-                            ([named, vec![item]].concat(), anon)
-                        },
-                        Signature::Anonymous(d) => {
-                            let item 
-                                = ListItem::new(format!("{:x}[{}]:{:>4}", 
-                                    d, age_summary, state.rssi));
-                            (named, [anon, vec![item]].concat())
-                        }
-                    }
-                });
-            let named_list = List::new(named_items)
-                .block(Block::default().title("Named").borders(Borders::ALL))
+            let now = Utc::now();
+            let (named_items, anon_items) 
+                = snapshot_to_list_items(&current_snapshot, &previous_snapshot, now);
+            let named_list = list(named_items, "Named");
+            let anon_list = list(anon_items, "Anonymous");
+            let (main_layout, snapshot_layout) = layout(f);
+            let runtime = format_duration((now - start).truncate_to_seconds().to_std().unwrap());
+            let footer = Paragraph::new(
+                    format!("Now: {now}\nRun time: {runtime}\n(press 'q' to quit)"))
+                .block(Block::default().title("Context").borders(Borders::ALL))
                 .style(Style::default().fg(Color::Black));
-            let anon_list = List::new(anon_items)
-                .block(Block::default().title("Anonymous").borders(Borders::ALL))
-                .style(Style::default().fg(Color::Black));
-            let chunks = Layout::default()
-                .direction(Direction::Horizontal)
-                .margin(1)
-                .constraints(
-                    [
-                        Constraint::Percentage(50),
-                        Constraint::Percentage(50)
-                    ].as_ref()
-                )
-                .split(f.size());
-            f.render_widget(named_list, chunks[0]);
-            f.render_widget(anon_list, chunks[1]);
+            f.render_widget(named_list, snapshot_layout[0]);
+            f.render_widget(anon_list, snapshot_layout[1]);
+            f.render_widget(footer, main_layout[1]);
         })?;
         if should_quit()? {
             break;
         }
         let events = scanner.scan().await?;
         state.discover(events);
+        previous_snapshot = current_snapshot;
     }
     Ok(())
 }
 
-// /// Render the application. This is where you would draw the application UI. This example just
-// /// draws a greeting.
-// fn render_app(f: &mut ratatui::Frame<CrosstermBackend<Stdout>>) {
-//     // let greeting = Paragraph::new("Hello World! (press 'q' to quit)");
-//     // frame.render_widget(greeting, frame.size());
-//     let chunks = Layout::default()
-//         .direction(Direction::Horizontal)
-//         .margin(1)
-//         .constraints(
-//             [
-//                 Constraint::Percentage(50),
-//                 Constraint::Percentage(50)
-//             ].as_ref()
-//         )
-//         .split(f.size());
-//     let block = Block::default()
-//          .title("Named")
-//          .borders(Borders::ALL);
-//     f.render_widget(block, chunks[0]);
-//     let block = Block::default()
-//          .title("Anonymous")
-//          .borders(Borders::ALL);
-//     f.render_widget(block, chunks[1]);
-// }
+fn snapshot_to_list_items<'a>(current: &Snapshot, previous: &Snapshot, now: DateTime<Utc>) -> (Vec<ListItem<'a>>, Vec<ListItem<'a>>) {
+    use humantime::format_duration;
+    use blescan::chrono_extra::Truncate;
+
+    let ordered = current.order_by_age_and_volume();
+    let compared_to_previous = ordered.compared_to(now, previous);
+    let (named_items, anon_items)   
+        = compared_to_previous.iter().fold((Vec::new(), Vec::new()), 
+            |
+                (named, anon), 
+                (state, comparison)
+            | {
+            let age_summary 
+                = format_duration(comparison.relative_age.truncate_to_seconds().to_std().unwrap());
+            let rssi_summary = match comparison.rssi {
+                RssiComparison::Louder => "↑",
+                RssiComparison::Quieter => "⌄",
+                RssiComparison::Same => "=",
+                RssiComparison::New => "*"
+            };
+            match &state.signature {
+                Signature::Named(n) => {
+                    let item 
+                        = ListItem::new(format!("{:<32}[{}]:{:>4}({})", 
+                            n, age_summary, state.rssi, rssi_summary));
+                    ([named, vec![item]].concat(), anon)
+                },
+                Signature::Anonymous(d) => {
+                    let item 
+                        = ListItem::new(format!("{:x}[{}]:{:>4}({})", 
+                            d, age_summary, state.rssi, rssi_summary));
+                    (named, [anon, vec![item]].concat())
+                }
+            }
+        });
+    (named_items, anon_items)   
+}
+
+fn list<'a>(items: Vec<ListItem<'a>>, title: &'a str) -> List<'a> {
+    List::new(items)
+        .block(Block::default().title(title).borders(Borders::ALL))
+        .style(Style::default().fg(Color::Black))
+}
+
+fn layout(frame: &mut Frame<'_, CrosstermBackend<Stdout>>) -> (Rc<[Rect]>, Rc<[Rect]>) {
+    let main_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Percentage(90),
+                Constraint::Percentage(10)
+            ].as_ref()
+        )
+        .split(frame.size());
+    let snapshot_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Percentage(50),
+                Constraint::Percentage(50)
+            ].as_ref()
+        )
+        .split(main_layout[0]);
+
+    (main_layout, snapshot_layout)
+}
 
 fn should_quit() -> Result<bool> {
     if event::poll(Duration::from_millis(250)).context("event poll failed")? {
--- blescan/src/snapshot.rs
+++ blescan/src/snapshot.rs
@@ -1,6 +1,8 @@
+use std::{collections::HashMap, cmp::Ordering};
+
 use crate::{ signature::Signature, device_state::DeviceState};
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Default, Clone)]
 pub struct Snapshot(pub Vec<DeviceState>);
 
 impl std::fmt::Display for Snapshot {
@@ -22,19 +24,43 @@ impl std::fmt::Display for Snapshot {
 }
 
 impl Snapshot {
-    #[must_use] pub fn order_by_age_oldest_last(&self) -> Snapshot {
-        let mut ordered_by_age : Vec<DeviceState> = self.0.clone();
-        ordered_by_age.sort_by(
-            |a, b| b.date_time.cmp(&a.date_time));
-        Snapshot(ordered_by_age)
+    #[must_use] pub fn order_by_age_and_volume(&self) -> Snapshot {
+        let mut ordered : Vec<DeviceState> = self.0.clone();
+        ordered.sort_by(
+            |a, b| 
+            if a.date_time == b.date_time {
+                b.rssi.cmp(&a.rssi)
+            }
+            else {
+                b.date_time.cmp(&a.date_time)
+            }
+        );
+        Snapshot(ordered)
     }
 
-    #[must_use] pub fn compared_to(&self, baseline: chrono::DateTime<chrono::Utc>) 
+    #[must_use] pub fn compared_to(&self, now: chrono::DateTime<chrono::Utc>, previous: &Snapshot) 
         -> Vec<(DeviceState, Comparison)> {
+        let previous_rssi: HashMap<Signature, i16> = previous.0.iter().map(|d| {
+            (d.signature.clone(), d.rssi)
+        }).collect();
         self.0.iter().map(|d| {
+            let curr = &d.rssi;
+            let rssi_comparison : RssiComparison  = match previous_rssi.get(&d.signature) {
+                Some(prev) => {
+                    match curr.cmp(prev) {
+                        Ordering::Greater => RssiComparison::Louder,
+                        Ordering::Equal => RssiComparison::Same,
+                        Ordering::Less => RssiComparison::Quieter
+                    }
+                },
+                None => RssiComparison::New
+            };
             (
                 d.clone(), 
-                Comparison { relative_age: d.date_time - baseline }
+                Comparison { 
+                    relative_age: now - d.date_time,
+                    rssi: rssi_comparison
+                }
             )
         }).collect()
     }
@@ -42,14 +68,23 @@ impl Snapshot {
 
 #[derive(PartialEq, Debug)]
 pub struct Comparison {
-    pub relative_age: chrono::Duration
+    pub relative_age: chrono::Duration,
+    pub rssi: RssiComparison
+}
+
+#[derive(PartialEq, Debug, Clone, Copy)]
+pub enum RssiComparison {
+    Louder,
+    Quieter,
+    Same,
+    New
 }
 
 #[cfg(test)]
 mod test {
     use chrono::{Utc, TimeZone, Duration};
 
-    use crate::{device_state::DeviceState, signature::Signature, snapshot::Comparison};
+    use crate::{device_state::DeviceState, signature::Signature, snapshot::{Comparison, RssiComparison}};
 
     use super::Snapshot;
 
@@ -67,27 +102,104 @@ mod test {
                 DeviceState::new(Utc.timestamp_opt(2, 0).unwrap(), Signature::Named("2".to_string()), -1),
                 DeviceState::new(Utc.timestamp_opt(1, 0).unwrap(), Signature::Named("1".to_string()), -1),
             ]);
-        let actual_order = initial_order.order_by_age_oldest_last();
+        let actual_order = initial_order.order_by_age_and_volume();
         assert_eq!(actual_order, expected_order);
     }
 
     #[test]
-    fn device_with_comparison() {
+    fn order_by_volume_when_same_age() {
+        let initial_order = 
+            Snapshot(vec![
+                DeviceState::new(Utc.timestamp_opt(3, 0).unwrap(), Signature::Named("1".to_string()), -3),
+                DeviceState::new(Utc.timestamp_opt(3, 0).unwrap(), Signature::Named("2".to_string()), -2),
+                DeviceState::new(Utc.timestamp_opt(3, 0).unwrap(), Signature::Named("3".to_string()), -1)
+            ]);
+        let expected_order = 
+            Snapshot(vec![
+                DeviceState::new(Utc.timestamp_opt(3, 0).unwrap(), Signature::Named("3".to_string()), -1),
+                DeviceState::new(Utc.timestamp_opt(3, 0).unwrap(), Signature::Named("2".to_string()), -2),
+                DeviceState::new(Utc.timestamp_opt(3, 0).unwrap(), Signature::Named("1".to_string()), -3)
+            ]);
+        let actual_order = initial_order.order_by_age_and_volume();
+        fn just_rssi(v: &[DeviceState]) -> Vec<i16> {
+            v.iter().map(|d|{ d.rssi}).collect()
+        }
+        assert_eq!(just_rssi(&actual_order.0), just_rssi(&expected_order.0));
+        assert_eq!(actual_order, expected_order);
+    }
+
+    #[test]
+    fn relative_age() {
         let snapshot = 
             Snapshot(vec![
                 DeviceState::new(Utc.timestamp_opt(1, 0).unwrap(), Signature::Named("1".to_string()), -1),
                 DeviceState::new(Utc.timestamp_opt(2, 0).unwrap(), Signature::Named("2".to_string()), -1),
                 DeviceState::new(Utc.timestamp_opt(3, 0).unwrap(), Signature::Named("3".to_string()), -1),
             ]);
-        let baseline = Utc.timestamp_opt(0, 0).unwrap();
+        let now = Utc.timestamp_opt(10, 0).unwrap();
         let expected_comparisons 
             = vec![
-                (snapshot.0[0].clone(), Comparison { relative_age: Duration::seconds(1) }),
-                (snapshot.0[1].clone(), Comparison { relative_age: Duration::seconds(2) }),
-                (snapshot.0[2].clone(), Comparison { relative_age: Duration::seconds(3) }),
+                (snapshot.0[0].clone(), Comparison { 
+                    relative_age: Duration::seconds(9),
+                    rssi: RssiComparison::New
+                }),
+                (snapshot.0[1].clone(), Comparison { 
+                    relative_age: Duration::seconds(8),
+                    rssi: RssiComparison::New 
+                }),
+                (snapshot.0[2].clone(), Comparison { 
+                    relative_age: Duration::seconds(7),
+                    rssi: RssiComparison::New
+                }),
             ];
         let actual_comparisons 
-            = snapshot.compared_to(baseline);
+            = snapshot.compared_to(now, &Snapshot::default());
+        assert_eq!(actual_comparisons, expected_comparisons);
+    }
+
+    #[test]
+    fn relative_volume() {
+        fn just_rssi(v: &[(DeviceState, Comparison)]) -> Vec<RssiComparison> {
+            v.iter().map(|(_,c)|{ c.rssi}).collect()
+        }
+        
+        let previous = 
+            Snapshot(vec![
+                DeviceState::new(Utc.timestamp_opt(1, 0).unwrap(), Signature::Named("1".to_string()), -10),
+                DeviceState::new(Utc.timestamp_opt(2, 0).unwrap(), Signature::Named("2".to_string()), -10),
+                DeviceState::new(Utc.timestamp_opt(3, 0).unwrap(), Signature::Named("3".to_string()), -10),
+            ]);
+        let now = Utc.timestamp_opt(10, 0).unwrap();
+        let current = 
+            Snapshot(vec![
+                DeviceState::new(Utc.timestamp_opt(1, 0).unwrap(), Signature::Named("1".to_string()), -5),
+                DeviceState::new(Utc.timestamp_opt(2, 0).unwrap(), Signature::Named("2".to_string()), -15),
+                DeviceState::new(Utc.timestamp_opt(3, 0).unwrap(), Signature::Named("3".to_string()), -10),
+                DeviceState::new(Utc.timestamp_opt(4, 0).unwrap(), Signature::Named("4".to_string()), -10),
+            ]);
+        let expected_comparisons 
+            = vec![
+                (current.0[0].clone(), Comparison { 
+                    relative_age: Duration::seconds(9),
+                    rssi: RssiComparison::Louder 
+                }),
+                (current.0[1].clone(), Comparison { 
+                    relative_age: Duration::seconds(8),
+                    rssi: RssiComparison::Quieter
+                }),
+                (current.0[2].clone(), Comparison { 
+                    relative_age: Duration::seconds(7),
+                    rssi: RssiComparison::Same 
+                }),
+                (current.0[3].clone(), Comparison { 
+                    relative_age: Duration::seconds(6),
+                    rssi: RssiComparison::New 
+                }),
+            ];
+        let actual_comparisons 
+            = current.compared_to(now, &previous);
+
+        assert_eq!(just_rssi(&actual_comparisons), just_rssi(&expected_comparisons));
         assert_eq!(actual_comparisons, expected_comparisons);
     }
 }
