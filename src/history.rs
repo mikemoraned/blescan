cbb008bb15add97e276b5173501bd14ca342e641
Mike Moran <mike@houseofmoran.com>
2023-08-11 14:22:08 +0200

    WIP

--- src/history.rs
+++ src/history.rs
@@ -2,22 +2,23 @@ use std::{path::Path, error::Error, io::Write, fs::OpenOptions};
 
 use crate::discover::DiscoveryEvent;
 
-pub struct EventSink(Box<dyn Write>);
+// pub struct EventSink<'a>(Box<dyn Write + 'a>);
+pub struct EventSink;
 
 impl EventSink {
     pub fn to_file<P>(path_arg: P) -> Result<EventSink, Box<dyn Error>> 
         where P: AsRef<Path>
     {
-        let path = path_arg.as_ref();
-        let w = OpenOptions::new()
-            .append(true)
-            .open(path)?;
-        Ok(EventSink(Box::new(w)))
+        // let path = path_arg.as_ref();
+        // let w = OpenOptions::new()
+        //     .append(true)
+        //     .open(path)?;
+        Ok(EventSink)
     }
 
-    // fn to_writer(w: dyn Write) -> EventSink {
-    //     EventSink(Box::new(w))
-    // }
+    fn to_writer(w: &mut dyn Write) -> EventSink {
+        EventSink
+    }
 
     pub fn save(&self, _events: &[DiscoveryEvent]) -> Result<(), Box<dyn Error>> {
         Ok(())
@@ -26,9 +27,25 @@ impl EventSink {
 
 #[cfg(test)]
 mod test {
+    use std::io::Cursor;
+
+    use chrono::{Utc, TimeZone};
+
+    use crate::{discover::DiscoveryEvent, signature::Signature};
+
+    use super::EventSink;
+
 
     #[test]
     fn sink_single_event() {
+        let events = &vec![
+            DiscoveryEvent::new(Utc.timestamp_opt(1, 0).unwrap(), Signature::Named("Device 1".to_string()), -20)
+        ];
+        let mut buf = Cursor::new(Vec::new());
+        let sink = EventSink::to_writer(&mut buf);
 
+        sink.save(&events).unwrap();
+
+        assert_eq!(buf.get_ref().is_empty(), false);
     }
 }
