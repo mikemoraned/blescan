cb8b5a60614f3fe28e6cb3b383959bc7cd6a11a8
Mike Moran <mike@houseofmoran.com>
2026-01-02 00:03:01 +0100

    add backlight

--- m5/m5-simple-graphics/src/main.rs
+++ m5/m5-simple-graphics/src/main.rs
@@ -1,3 +1,8 @@
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::peripherals::Peripherals;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -7,4 +12,19 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    // Get peripherals
+    let peripherals = Peripherals::take().unwrap();
+
+    // GPIO27 controls the backlight on M5StickC Plus2
+    let mut backlight = PinDriver::output(peripherals.pins.gpio27).unwrap();
+
+    // Turn backlight on
+    backlight.set_high().unwrap();
+    log::info!("Backlight turned on");
+
+    // Keep the backlight on
+    loop {
+        thread::sleep(Duration::from_secs(1));
+    }
 }
